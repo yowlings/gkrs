e3313841d370a0495a775a55365af1838b4acd3e
wqhash <wqhash@163.com>
2017-08-22 09:21:12 +0800

    dev

--- script/lib/about.js
+++ script/lib/about.js
@@ -1,39 +1,72 @@
-/**
- * Created by wqhash on 2017/8/20.
- */
-//创建和初始化地图函数：
-function initMap(){
-    createMap();//创建地图
-    setMapEvent();//设置地图事件
-    addMapControl();//向地图添加控件
-    addMapOverlay();//向地图添加覆盖物
+// 百度地图API功能
+var mp = new BMap.Map("allmap");
+//mp.centerAndZoom(new BMap.Point(116.3964,39.9093), 15);
+mp.centerAndZoom(new BMap.Point(116.342614,39.98568), 18);
+mp.enableScrollWheelZoom();
+// 复杂的自定义覆盖物
+function ComplexCustomOverlay(point, text, mouseoverText){
+    this._point = point;
+    this._text = text;
+    this._overText = mouseoverText;
 }
-function createMap(){
-    map = new BMap.Map("map");
-    map.centerAndZoom(new BMap.Point(116.35347,39.986069),15);
+ComplexCustomOverlay.prototype = new BMap.Overlay();
+ComplexCustomOverlay.prototype.initialize = function(map){
+    this._map = map;
+    var div = this._div = document.createElement("div");
+    div.style.position = "absolute";
+    div.style.zIndex = BMap.Overlay.getZIndex(this._point.lat);
+    div.style.backgroundColor = "#EE5D5B";
+    div.style.border = "1px solid #BC3B3A";
+    div.style.color = "white";
+    div.style.height = "18px";
+    div.style.padding = "2px";
+    div.style.lineHeight = "18px";
+    div.style.whiteSpace = "nowrap";
+    div.style.MozUserSelect = "none";
+    div.style.fontSize = "12px"
+    var span = this._span = document.createElement("span");
+    div.appendChild(span);
+    span.appendChild(document.createTextNode(this._text));
+    var that = this;
+
+    var arrow = this._arrow = document.createElement("div");
+    arrow.style.background = "url(http://map.baidu.com/fwmap/upload/r/map/fwmap/static/house/images/label.png) no-repeat";
+    arrow.style.position = "absolute";
+    arrow.style.width = "11px";
+    arrow.style.height = "10px";
+    arrow.style.top = "22px";
+    arrow.style.left = "10px";
+    arrow.style.overflow = "hidden";
+    div.appendChild(arrow);
+
+    div.onmouseover = function(){
+        this.style.backgroundColor = "#6BADCA";
+        this.style.borderColor = "#0000ff";
+        this.getElementsByTagName("span")[0].innerHTML = that._overText;
+        arrow.style.backgroundPosition = "0px -20px";
+    }
+
+    div.onmouseout = function(){
+        this.style.backgroundColor = "#EE5D5B";
+        this.style.borderColor = "#BC3B3A";
+        this.getElementsByTagName("span")[0].innerHTML = that._text;
+        arrow.style.backgroundPosition = "0px 0px";
+    }
+
+    mp.getPanes().labelPane.appendChild(div);
+
+    return div;
 }
-function setMapEvent(){
-    map.enableScrollWheelZoom();
-    map.enableKeyboard();
-    map.enableDragging();
-    map.enableDoubleClickZoom()
+ComplexCustomOverlay.prototype.draw = function(){
+    var map = this._map;
+    var pixel = map.pointToOverlayPixel(this._point);
+    this._div.style.left = pixel.x - parseInt(this._arrow.style.left) + "px";
+    this._div.style.top  = pixel.y - 30 + "px";
 }
-function addClickHandler(target,window){
-    target.addEventListener("click",function(){
-        target.openInfoWindow(window);
-    });
-}
-function addMapOverlay(){
-}
-//向地图添加控件
-function addMapControl(){
-    var scaleControl = new BMap.ScaleControl({anchor:BMAP_ANCHOR_BOTTOM_LEFT});
-    scaleControl.setUnit(BMAP_UNIT_IMPERIAL);
-    map.addControl(scaleControl);
-    var navControl = new BMap.NavigationControl({anchor:BMAP_ANCHOR_TOP_LEFT,type:BMAP_NAVIGATION_CONTROL_LARGE});
-    map.addControl(navControl);
-    var overviewControl = new BMap.OverviewMapControl({anchor:BMAP_ANCHOR_BOTTOM_RIGHT,isOpen:true});
-    map.addControl(overviewControl);
-}
-var map;
-initMap();
+//var txt = "银湖海岸城", mouseoverTxt = txt + " " + parseInt(Math.random() * 1000,10) + "套" ;
+var txt = "中国科学院软件园区", mouseoverTxt = txt + " "  ;
+
+//var myCompOverlay = new ComplexCustomOverlay(new BMap.Point(116.407845,39.914101), "银湖海岸城",mouseoverTxt);
+var myCompOverlay = new ComplexCustomOverlay(new BMap.Point(116.342614,39.98568), "中国科学院软件园区",mouseoverTxt);
+
+mp.addOverlay(myCompOverlay);
